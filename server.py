93fd772ecb90fc59df0c02f4d94ad4beae5e5ecd
Ben Bauer <ben.bauer1@gmail.com>
2021-08-04 10:49:57 -0500

    created paths with variable input

--- server.py
+++ server.py
@@ -1,32 +1,43 @@
-# from flask import Flask
+from flask import Flask
 import time
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/")
-# def helloworld():
-#     return 'hello world'
+@app.route("/")
+def helloworld():
+    return 'hello world'
+
+@app.route("/bye")
+def byeworld():
+    return 'bye bye bye!!'
+
+@app.route("/<name>")
+def greet(name):
+    return f"Hello, {name}"
 
     ##python decorator functino
     #delay_decorator causes a 2 second pause before running
     #adding @delay_decorator will before defining the function will cause the
     #decorator function to run first followed by the defined function when the defined function is called.
-def delay_decorator(function):
-    def wrapperfunction():
-        time.sleep(2)
-        function()
-    return wrapperfunction
+# def delay_decorator(function):
+#     def wrapperfunction():
+#         time.sleep(2)
+#         function()
+#     return wrapperfunction
 
-@delay_decorator
-def sayHello():
-    print("hello")
+# @delay_decorator
+# def sayHello():
+#     print("hello")
 
-@delay_decorator
-def saybye():
-    print("bye bye bye")
+# @delay_decorator
+# def saybye():
+#     print("bye bye bye")
 
-def saygreets():
-    print("greets")
+# def saygreets():
+#     print("greets")
 
-sayHello()
-saybye()
-saygreets()
+# sayHello()
+# saybye()
+# saygreets()
+
+if __name__ == "__main__":
+    app.run(debug=True)
